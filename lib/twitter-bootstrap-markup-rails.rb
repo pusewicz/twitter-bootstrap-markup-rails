5534a1b5bbe7a9bde7aab7fa7e70668362f6d9cf
Piotr Usewicz <piotr@layer22.com>
2012-01-19 10:47:48 +0000

    Remove 1.9.2 specific require_relative

--- lib/twitter-bootstrap-markup-rails.rb
+++ lib/twitter-bootstrap-markup-rails.rb
@@ -1,13 +1,15 @@
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
+
 module Twitter
   module Bootstrap
     module Markup
       module Rails
-        require_relative "twitter-bootstrap-markup-rails/version"
+        require "twitter-bootstrap-markup-rails/version"
 
         autoload :Helpers, "twitter-bootstrap-markup-rails/helpers"
         autoload :Components, "twitter-bootstrap-markup-rails/components"
 
-        require_relative "twitter-bootstrap-markup-rails/engine" if defined?(::Rails)
+        require "twitter-bootstrap-markup-rails/engine" if defined?(::Rails)
       end
     end
   end
