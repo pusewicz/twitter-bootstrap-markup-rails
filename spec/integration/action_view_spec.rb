5128bd2b94fe8c27ff150acdb3ab4291c779fec4
Piotr Usewicz <piotr@layer22.com>
2012-04-18 14:42:59 -0700

    Correct name of the spec

--- spec/integration/action_view_spec.rb
+++ spec/integration/action_view_spec.rb
@@ -17,7 +17,7 @@ describe ActionView::Base do
     @view.ancestors.should include(Twitter::Bootstrap::Markup::Rails::Helpers::FormHelpers)
   end
 
-  it "includes inline form helpers" do
+  it "includes inline navigation helpers" do
     @view.ancestors.should include(Twitter::Bootstrap::Markup::Rails::Helpers::NavigationHelpers)
   end
 end
