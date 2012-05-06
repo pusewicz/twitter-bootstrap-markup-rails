02f5fb79a17a2bbbb6f8579376bc617bb667f894
Cristian Bica <cristian.bica@gmail.com>
2012-05-07 01:37:22 +0300

    Fixed documentation

--- lib/twitter-bootstrap-markup-rails/helpers/modal_helpers.rb
+++ lib/twitter-bootstrap-markup-rails/helpers/modal_helpers.rb
@@ -5,12 +5,12 @@ module Twitter::Bootstrap::Markup::Rails::Helpers
 
     # Render a modal popup
     #
-    # @param [Hash] options hash containing options (default: {}):
-    #            :dom_id       - DOM ID of the modal element (mandatory)
-    #            :fade         - if true, add the fade class to the modal DOM element so the modal will be animated when shown
-    #            :header_title - modal title
-    # Examples
+    # @param [Hash] options hash containing options (default: { :dom_id => 'twitter-bootstrap-modal', :fade => false, :header_title => '' }):
+    #            :dom_id       - DOM ID of the modal element (mandatory).
+    #            :fade         - if true, add the fade class to the modal DOM element so the modal will be animated when shown.
+    #            :header_title - modal title.
     #
+    # @example
     #   bootstrap_modal(dom_id: 'a_dom_id', fade: true, header_title: "I'm a bootstrap modal popup") do |modal|
     #     modal.body do |c|
     #      c."some content"
@@ -20,8 +20,8 @@ module Twitter::Bootstrap::Markup::Rails::Helpers
     #       f.bootstrap_button "Cancel", "url"
     #     end
     #   end
-    # with HAML
     #
+    # @example With HAML
     #   =bootstrap_modal(dom_id: 'a_dom_id', fade: true, header_title: "I'm a bootstrap modal popup") do |modal|
     #     -modal.body do |c|
     #       -c.content_tag :div do
@@ -31,15 +31,17 @@ module Twitter::Bootstrap::Markup::Rails::Helpers
     #     -modal.footer do |f|
     #       -f.bootstrap_button "Save", "url", :type => 'btn-primary'
     #       -f.bootstrap_button "Cancel", "url"
-    # to render the button to show the modal use:
-    #   =bootstrap_button "Show Modal", "#a_dom_id", :html_options => {:"data-toggle" => "modal"}
     #
+    # @example To render the button to show the modal use:
+    #   =bootstrap_button "Show Modal", "#a_dom_id", :html_options => {:"data-toggle" => "modal"}
     #
     # Returns HTML String for the modal popup
     
     def bootstrap_modal(options = {})
       elements = Twitter::Bootstrap::Markup::Rails::HelperCollectionSet.new(self, [:body, :footer])
+      
       yield elements
+      
       Twitter::Bootstrap::Markup::Rails::Components::Modal.new(
         elements.collections[:body],
         elements.collections[:footer],
